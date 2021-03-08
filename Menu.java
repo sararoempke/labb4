223f0f5db21491f9451c0ab4deb5a525a1584e82
sararoempke <77969504+sararoempke@users.noreply.github.com>
2021-03-08 10:53:13 +0100

    Update Menu.java
    ffa

--- Menu.java
+++ Menu.java
@@ -1,12 +1,25 @@
 package view;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+import javafx.scene.control.Alert;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 
 /**
@@ -14,21 +27,13 @@ import javafx.scene.control.Button;
  *
  * @author sarar
  */
-public class Menu extends Application {
+public class Menu extends Application /*implements EventHandler<ActionEvent>*/ {
+    private final Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    ImageView myImageView;
+    
     @Override
     public void start(Stage primaryStage){
-        BorderPane pane = new BorderPane();
-        pane.setLeft(getVBox());
         
-        Scene scene = new Scene(pane, 700, 600);
-        primaryStage.setTitle("Testing testing");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        
-       
-    }
-    
-    private VBox getVBox(){
         VBox vBox = new VBox(15);
         vBox.setPadding(new Insets(30, 10, 10, 10));
         Button btAddPic = new Button("Add Image");
@@ -38,10 +43,64 @@ public class Menu extends Application {
         Button btHistogram = new Button("View Histogram");
         Button btSavePic = new Button("Save Image");
         vBox.getChildren().addAll(btAddPic, btInvertColor, btBlurPic, btContrast, btHistogram, btSavePic);
-        return vBox;
-    }
+        
+        FileChooser filechoice = new FileChooser();
+        myImageView = new ImageView();
+        
+                btAddPic.setOnAction( new EventHandler<ActionEvent>(){
+                    @Override
+                    public void handle(ActionEvent event) {
+                System.out.println("Vill addera en image");
+                FileChooser fileChoice = new FileChooser();
+
+                FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
+                FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
+                fileChoice.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
+
+                File file = fileChoice.showOpenDialog(null);
+
+                try{
+                    BufferedImage bufferedImage = ImageIO.read(file);
+                    Image image = SwingFXUtils.toFXImage(bufferedImage, null);
+                    myImageView.setImage(image);
+                }catch (IOException ex) {
+
+
+                }
+                    }
+            }
+
+                );
+
+
+            BorderPane pane = new BorderPane();
+            pane.setLeft(vBox);
+            pane.setRight(myImageView);
+
+            Scene scene = new Scene(pane, 700, 600);
+            primaryStage.setTitle("Testing testing");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+       
+        
+       
+        }
+    
+    
+    
     public static void main(String[] args) {
         launch(args);
     }
     
+    
+    void showAlert(String message) {
+        alert.setHeaderText("");
+        alert.setTitle("Alert!");
+        alert.setContentText(message);
+        alert.show();
+    }
+    
+    
 }
+
