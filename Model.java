89f71c0d0b75551776b1fb6c88811634b02c8e71
sararoempke <77969504+sararoempke@users.noreply.github.com>
2021-03-13 12:01:12 +0100

    Update Model.java
    Det är ju Main, inte model , vet bara inte hur man fixar en ny fil...

--- Model.java
+++ Model.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Model;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import view.myView;
+import model.Model;
 
 /**
  *
- * @author moawahlgren
+ * @author sarar
  */
-public class Model {
+public class MainAgain extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        // create model, view and controller, and tie them together
+        Model model = new Model();
+        myView view = new myView(model); // also creates the controller
+        
+        
+        Scene scene = new Scene(view, 700, 600);
+        primaryStage.setTitle("Image Processing");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+    }
+    
+    public static void main(String[] args){
+        launch(args);
+    }
     
 }
+
+}
+
+//den här satte jag i default packade, men det spelar säkert ingen roll
