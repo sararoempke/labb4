a690b6f941444ea3a9e910364418be848f592036
sararoempke <77969504+sararoempke@users.noreply.github.com>
2021-03-14 11:40:59 +0100

    Update Menu.java
    myView

--- Menu.java
+++ Menu.java
@@ -1,5 +1,6 @@
 package view;
 
+import controller.Controller;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -16,48 +17,61 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import model.Model;
+import controller.Controller;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javax.imageio.ImageIO;
 
 public class myView extends BorderPane {
 
     private final Model model;
-    private final BorderPane pane;
-    public ImageView myImageView;
-    //private final Controller controller;
+    public static BorderPane pane;
+    public ImageView myImageView = null;
+    public boolean showSlider = false;
+    public static int windowValue;
+    public static int levelValue;
 
     
     private final Alert alert = new Alert(Alert.AlertType.INFORMATION);
 
     public myView(Model model) {
         this.model = model;
-        //controller = new Controller(model, this);
+        
         
         pane = new BorderPane();
         myImageView = new ImageView();
-      
-        MenuBar menuBar = createMenu();//arg controller??
+        Controller controller = new Controller(model, this);
+        MenuBar menuBar = createMenu(controller);
 	
         this.setTop(menuBar);
         this.setRight(myImageView);
-
-        //updateFromModel();
+        
+        if(showSlider == true){
+            VBox sliders = contrastView();
+            this.setLeft(sliders);
+        }
     }    
     
-    private MenuBar createMenu() {//arg controller??
+    private MenuBar createMenu(Controller controller) {//arg controller??
 		MenuItem exitItem = theExit();
-                MenuItem savePic = theSave();
+                MenuItem savePic = theSave(controller);
                 MenuItem openPic = toOpenImage();
 		
 		Menu fileMenu = new Menu("File");
 		fileMenu.getItems().addAll(exitItem, savePic, openPic);
                 
-                MenuItem blurPic = blurImage();
-                MenuItem invertColor = toInvertColor();
-                MenuItem histogram = toOpenHistogram();
+                MenuItem blurPic = blurImage(controller);
+                MenuItem invertColor = toInvertColor(controller);
+                MenuItem histogram = toOpenHistogram(controller);
+                MenuItem editContrast = toEditContrast(controller);
                 Menu processMenu = new Menu("Process");
-                processMenu.getItems().addAll(histogram, blurPic, invertColor);
+                processMenu.getItems().addAll(histogram, blurPic, invertColor, editContrast);
                 
                 if(myImageView == null){
                     processMenu.hide();
@@ -69,6 +83,48 @@ public class myView extends BorderPane {
 		menuBar.getMenus().addAll(fileMenu, processMenu);
 		
 		return menuBar;
+    }
+    
+    public VBox contrastView(){
+        VBox sliders = new VBox(10);
+            Label windowlb = new Label("Window:");
+            Label levellb = new Label("Level:");
+            Slider Window = new Slider(0, 255, 0);
+            Window.setShowTickLabels(true);
+            Window.setShowTickMarks(true);
+            Slider Level = new Slider(0, 255, 0);
+            Level.setShowTickLabels(true);
+            Level.setShowTickMarks(true);
+            //Button btEdit = new Button("Edit");
+            Button btDone = new Button("Apply");
+            
+            Window.valueProperty().addListener(new ChangeListener<Number>(){
+                @Override
+                public void changed(ObservableValue <? extends Number >
+                        observable, Number oldValue, Number newValue){
+                    windowValue = (int) newValue;
+                }
+            });
+            
+            Level.valueProperty().addListener(new ChangeListener<Number>(){
+                @Override
+                public void changed(ObservableValue <? extends Number >
+                        observable, Number oldValue, Number newValue){
+                    levelValue = (int) newValue;
+                }
+            });
+            
+            btDone.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    showSlider = false;
+                }
+            });
+            
+            sliders.getChildren().addAll(windowlb, Window, levellb, Level, btDone);
+            
+            return sliders;
+        
     }
     
     // ska fråga om du inte vill spara bild först
@@ -84,13 +140,13 @@ public class myView extends BorderPane {
         return exitItem;
     }
     
-    public MenuItem theSave(){
+    public MenuItem theSave(Controller controller){
         MenuItem savePic = new MenuItem("Save Image");
         savePic.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Save the image");
-                Controller.handleSavePic(myImageView.getImage());
+                controller.handleSavePic(myImageView.getImage());
             }
         });
         
@@ -120,41 +176,57 @@ public class myView extends BorderPane {
         return openPic;
     }
     
-    public MenuItem toInvertColor(){
+    public MenuItem toInvertColor(Controller controller){
         MenuItem invertColor = new MenuItem("Invert Color");
         invertColor.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Invert Color");
-                Image newImage = Controller.handleInvertColor(myImageView.getImage()); 
+                Image newImage = controller.handleInvertColor(myImageView.getImage()); 
                 myImageView.setImage(newImage);
             }
         });
         return invertColor;
     }
     
-    public MenuItem blurImage(){
+    public MenuItem blurImage(Controller controller){
         MenuItem blurPic = new MenuItem("Blur");
         blurPic.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Da blurr");
-                Image newImage = Controller.handleBlurPic(myImageView.getImage()); 
+                Image newImage = controller.handleBlurPic(myImageView.getImage()); 
                 myImageView.setImage(newImage);
             }
         });
         return blurPic;
     }
     
-    public MenuItem toOpenHistogram(){
+    public MenuItem toOpenHistogram(Controller controller){
         MenuItem histogram = new MenuItem("Show Histogram");
         histogram.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
+                //showSlider = true;
                 System.out.println("Show Histogram");
+                //showSlider = false;
             }
         });
         return histogram;
+    }
+    
+    public MenuItem toEditContrast(Controller controller){
+        MenuItem editContrast = new MenuItem("Edit Contrast");
+        editContrast.setOnAction(new EventHandler<ActionEvent>(){
+            @Override
+            public void handle(ActionEvent event) {
+                showSlider = true;
+                System.out.println("Editing contrast");
+                Image newImage = controller.handleContrast(myImageView.getImage()); 
+                myImageView.setImage(newImage);
+            }
+        });
+        return editContrast;
     }
 
 }
