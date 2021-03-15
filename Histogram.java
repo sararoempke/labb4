129600ed7dc2a2e2d5b4434ae9e624d618a9d50c
sararoempke <77969504+sararoempke@users.noreply.github.com>
2021-03-15 13:47:48 +0100

    Update Histogram.java
    fsdsfds

--- Histogram.java
+++ Histogram.java
@@ -10,5 +10,42 @@ package Model;
  * @author moawahlgren
  */
 public class Histogram {
+
+public void displayHistogram(int[][] matrix){
+     
+        final NumberAxis xAxel = new NumberAxis();
+        final NumberAxis yAxel = new NumberAxis();
+        final LineChart lineChart = new LineChart(xAxel, yAxel);
+        lineChart.setTitle("Histogram");
+        
+        /**/
+        //defining series
+        
+        XYChart.Series seriesRed = new XYChart.Series();
+        XYChart.Series seriesGreen = new XYChart.Series();
+        XYChart.Series seriesBlue = new XYChart.Series();
+        
+        seriesRed.setName("Red");
+        seriesGreen.setName("Green");
+        seriesBlue.setName("Blue");
+        
+     
+        
+            
+                for (int j=0; j<matrix[0].length; j++) {
+                    // lägga data i series
+                    
+                    seriesRed.getData().add(new XYChart.Data(j, matrix[0][j]));
+                    seriesGreen.getData().add(new XYChart.Data(j, matrix[1][j]));
+                    seriesBlue.getData().add(new XYChart.Data(j, matrix[2][j]));
+                   
+                }
+        
+            
+        
+        
+        
+    }
+   
     
 }
